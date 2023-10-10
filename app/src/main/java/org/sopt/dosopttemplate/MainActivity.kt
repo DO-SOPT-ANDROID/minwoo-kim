18d3f9581095b2e6c5043a98e83f728aef7b74cd
Do-Re-Min-Woo <kimy5878@naver.com>
2023-10-10 21:23:42 +0900

    [FEAT/#1] 메인 화면 및 기능 구현

--- app/src/main/java/org/sopt/dosopttemplate/MainActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/MainActivity.kt
@@ -2,10 +2,26 @@ package org.sopt.dosopttemplate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.sopt.dosopttemplate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val id = intent.getStringExtra("Id")
+        val pw = intent.getStringExtra("Pw")
+        val nickName = intent.getStringExtra("NickName")
+        val address = intent.getStringExtra("Address")
+
+        binding.run {
+            tvMainNickName.text = nickName
+            tvMainId.text = id
+            tvMainAddress.text = address
+        }
     }
 }
