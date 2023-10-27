441ae82f6855885e5ff86b886e261f048d3182b1
Do-Re-Min-Woo <kimy5878@naver.com>
2023-10-27 22:33:42 +0900

    [FEAT/#5] 멀티 뷰 타입 리사이클러뷰 구현

---
+++ app/src/main/java/org/sopt/dosopttemplate/adapter/FriendInfoViewHolder.kt
@@ -0,0 +1,18 @@
+package org.sopt.dosopttemplate.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import org.sopt.dosopttemplate.databinding.ItemFriendInfoBinding
+import org.sopt.dosopttemplate.model.data.Friend
+
+class FriendInfoViewHolder(private val binding: ItemFriendInfoBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    fun onBind(friendInfoData: Friend.FriendInfo) {
+        binding.run {
+            ivFriendInfoProfile.setImageResource(friendInfoData.profileImage)
+            tvFriendInfoName.text = friendInfoData.name
+            tvFriendInfoDescription.text = friendInfoData.description
+            tvFriendMusicTitle.text = friendInfoData.music
+        }
+    }
+}
