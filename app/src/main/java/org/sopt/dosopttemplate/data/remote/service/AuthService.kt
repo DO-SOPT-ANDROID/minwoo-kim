0ea8ac3a30fa14c53e0436dfc41c1d702b640d31
Do-Re-Min-Woo <kimy5878@naver.com>
2023-12-08 18:56:26 +0900

    [MOD/#13] 코루틴 적용으로 코드 수정

--- app/src/main/java/org/sopt/dosopttemplate/data/remote/service/AuthService.kt
+++ app/src/main/java/org/sopt/dosopttemplate/data/remote/service/AuthService.kt
@@ -3,15 +3,17 @@ package org.sopt.dosopttemplate.data.remote.service
 import org.sopt.dosopttemplate.data.remote.model.dto.request.auth.RequestLoginDto
 import org.sopt.dosopttemplate.data.remote.model.dto.request.auth.RequestSignupDto
 import org.sopt.dosopttemplate.data.remote.model.dto.response.auth.ResponseLoginDto
+import org.sopt.dosopttemplate.data.remote.model.dto.response.auth.ResponseSignupDto
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AuthService {
     @POST("api/v1/members")
-    fun postSignup(
+    suspend fun postSignup(
         @Body request: RequestSignupDto,
-    ): Call<Unit>
+    ): Response<ResponseSignupDto>
 
     @POST("api/v1/members/sign-in")
     fun postLogin(
