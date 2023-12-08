b464f0708dc146b6a27cf4bc048a1a08b74ebba1
Do-Re-Min-Woo <kimy5878@naver.com>
2023-12-08 18:56:09 +0900

    [FEAT/#13] ViewModel 분리

--- app/src/main/java/org/sopt/dosopttemplate/ui/auth/SignUpActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/ui/auth/SignUpActivity.kt
@@ -1,58 +1,74 @@
 package org.sopt.dosopttemplate.ui.auth
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import org.sopt.dosopttemplate.data.local.UserInfo
-import org.sopt.dosopttemplate.data.remote.api.ServicePool
-import org.sopt.dosopttemplate.data.remote.model.dto.request.auth.RequestSignupDto
 import org.sopt.dosopttemplate.databinding.ActivitySignUpBinding
 import org.sopt.dosopttemplate.util.base.BindingActivity
 import org.sopt.dosopttemplate.util.context.shortToast
-import retrofit2.Call
-import retrofit2.Response
 
 class SignupActivity :
     BindingActivity<ActivitySignUpBinding>({ ActivitySignUpBinding.inflate(it) }) {
-    private val signupViewModel by viewModels<SignupViewModel>()
+    private val viewModel by viewModels<SignupViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         binding.lifecycleOwner = this
-        binding.signupViewModel = signupViewModel
+        binding.signupViewModel = viewModel
 
         hideKeyBoard()
-        initSignupBtn()
+        onClickSignupBtn()
+        observeSignupValid()
+        observeSignupState()
     }
 
-    private fun initSignupBtn() {
-        binding.run {
-            btnSignUpToSignUp.setOnClickListener {
-                val id = etSignUpId.text.toString()
-                val password = etSignUpPw.text.toString()
-                val nickname = etSignNickName.text.toString()
-                val address = etSignAddress.text.toString()
-                val userInfo = UserInfo(id, password, nickname, address)
+    private fun onClickSignupBtn() {
+        binding.btnSignUpToSignUp.setOnClickListener {
+            viewModel.postSignup()
+        }
+    }
 
-                ServicePool.authService.postSignup(RequestSignupDto(id, password, nickname))
-                    .enqueue(object : retrofit2.Callback<Unit> {
-                        override fun onResponse(
-                            call: Call<Unit>,
-                            response: Response<Unit>,
-                        ) {
-                            if (response.isSuccessful) {
-                                shortToast("회원가입 성공")
-                                intent.putExtra("UserInfo", userInfo)
-                                setResult(RESULT_OK, intent)
-                                finish()
-                            }
-                        }
+    private fun observeSignupValid() {
+        viewModel.isIdValid.observe(this) { isIdValid ->
+            if (!isIdValid && !viewModel.id.value.isNullOrBlank()) {
+                binding.tilSignupId.isErrorEnabled = true
+                binding.tilSignupId.error = "영문, 숫자 포함 6~10자"
+            } else {
+                binding.tilSignupId.isErrorEnabled = false
+            }
+            viewModel.signupBtnEnabled()
+        }
+        viewModel.isPwValid.observe(this) { isPwValid ->
+            if (!isPwValid && !viewModel.pw.value.isNullOrBlank()) {
+                binding.tilSignupPw.isErrorEnabled = true
+                binding.tilSignupPw.error = "영문, 숫자, 특수문자 포함 6~12자"
+            } else {
+                binding.tilSignupPw.isErrorEnabled = false
+            }
+            viewModel.signupBtnEnabled()
+        }
+    }
 
-                        override fun onFailure(call: Call<Unit>, t: Throwable) {
-                            shortToast("서버 에러 발생")
-                        }
-                    })
+    private fun observeSignupState() {
+        viewModel.signupState.observe(this) { signupState ->
+            when (signupState) {
+                is SignupState.Success -> {
+                    shortToast("회원가입 성공")
+                    intent.putExtra("UserInfo", signupState.data)
+                    setResult(RESULT_OK, intent)
+                    finish()
+                }
+
+                is SignupState.Error -> {
+                    shortToast("회원가입 실패 : ${signupState.message}")
+                }
+
+                is SignupState.Loading -> {
+                    shortToast("회원가입 중")
+                }
             }
         }
     }
